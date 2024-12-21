3f5aa0ef402f4c9a803f6522f9d9ed6818d25586
kunalg12 <kunalgursal2923@gmail.com>
2024-12-21 22:52:09 +0530

    updaate

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -17,14 +17,6 @@ export default function index(props, projects) {
                     </div>
                 </div>
             </div>
-            <Head title="Projects" />
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">Projects!</div>
-                    </div>
-                </div>
-            </div>
         </AuthenticatedLayout>
     )
 }

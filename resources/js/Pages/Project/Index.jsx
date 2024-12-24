8005bd650c8927000d630372591fe2996e2c6e77
kunalg12 <kunalgursal2923@gmail.com>
2024-12-24 23:58:23 +0530

    project index

---
+++ resources/js/Pages/Project/Index.jsx
@@ -0,0 +1,29 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+
+export default function Index({ auth, errors, projects }) {
+    return (
+        <AuthenticatedLayout
+            auth={auth}
+            errors={errors}
+            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Projects</h2>}
+        >
+
+            <Head title="Projects" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <ul>
+                                {projects.map((project, index) => (
+                                    <li key={index}>{project.name}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    )
+}
+
